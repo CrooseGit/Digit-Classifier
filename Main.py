0219af1a64a9d1fb3c7cc7ca3caf6ab43c488499
CrooseGit <reubenfc@gmail.com>
2024-06-05 14:18:06 +0100

    finished tutorial on gaining access to mnist

--- Main.py
+++ Main.py
@@ -4,5 +4,10 @@ from keras.datasets import mnist
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 # 60k training images, 10k test images. Images are 28 x 28
 
+from matplotlib import pyplot
+for i in range(9):
+    pyplot.subplot(330+1+i)
+    pyplot.imshow(train_X[i], cmap = pyplot.get_cmap('gray'))
+pyplot.show()
 
 print("Hello world")
